b149303eef7efd263a4240a56c5def3174a59fa6
frascu <francescoscuccimarri@gmail.com>
2020-10-26 17:39:50 +0100

    minor changes

--- src/main/java/it/scuccimarri/controllers/IndexController.java
+++ src/main/java/it/scuccimarri/controllers/IndexController.java
@@ -8,6 +8,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "index"})
     public String getIndexPage() {
+        System.out.println("Recipe Application - 123");
         return "index";
     }
 }
